385d08028b0e36c01236a14109f3af708506d346
Andrew Seto <andrewseto@live.com>
2017-08-15 22:23:01 -0400

    Refactored toward more OOP-y

--- src/moodText/MoodText.java
+++ src/moodText/MoodText.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 enum MOOD {POSITIVE,NEGATIVE,NEUTRAL};
+
 public class MoodText {
+	
+	static final List<String> negateWords = Arrays.asList("not","dont","never","no");
+	
 	static MOOD mood = MOOD.NEUTRAL;
 	public static Map<String, Integer> loadWordBank(){
 		Map <String,Integer> loadingMap = new HashMap<String,Integer>();
@@ -86,81 +92,114 @@ public class MoodText {
 	
 			
 	}
-	
-	public static void main(String[] args) {
+	public static MOOD getEmotion(List<String> list){
 		Map <String, Integer> wordBank = loadWordBank();
+		double absTotal=0;
+		double total=0;
+		int negate = 1;
+
+		for(String word:list){
+			System.out.println(word);
+		}
 		
-		FileReader fr;
-		
-		try {
-			fr = new FileReader("story.txt");
-			BufferedReader br = new BufferedReader (fr);
-			String wholeLine;
-			List<String> negateWords = Arrays.asList("not","dont","never","no");
-			List<Double> phraseSum = new ArrayList<Double>();
-			double absTotal=0;
-			lines: while ((wholeLine=br.readLine())!=null){
-		        String parsedLine = wholeLine.replaceAll("[^a-zA-Z\\s]", "").replaceAll("\\s+", " ");
-				parsedLine = parsedLine.toLowerCase();
-		        String[] words = parsedLine.split(" ");
-		        
-		        
-				int negate = 1;
-				//first one to count gets negate
-				phrases: for(int r=0;r<words.length;r++){
-					double perphraseSum = 0;					
+		for(String word:list){
+			if (negateWords.contains(word)){
+				System.out.println("N: " + word);
+				negate = -1;
+			}		
+			else if (wordBank.containsKey(word)){
+				double wordRating = wordBank.get(word)*negate;
 
-					
-					
-					if (negateWords.contains(words[r])){
-						System.out.println("N: " + words[r]);
-						negate = -1;
-					}
-					else if (wordBank.containsKey(words[r])){
-						double wordRating = wordBank.get(words[r])*negate;
-
-						System.out.println("M: " + words[r] + wordRating);
-
-						absTotal = absTotal + Math.abs(wordRating);
-						perphraseSum = perphraseSum + wordRating;
-						if (negate == -1) negate = 1;
-						phraseSum.add(perphraseSum);
-					}
-
-				
-				}
+				System.out.println("M: " + word + wordRating);
+				total=total+wordRating;
+				absTotal = absTotal + Math.abs(wordRating);
+				if (negate == -1) negate = 1;
 			}
-			double total=0;
-			for(double r : phraseSum){
-				total = total + r;
-			}
-			double score = total/absTotal;
-			System.out.println(score);
-			if (score<-0.25) {
-				System.out.println("NEGATIVE");
-				openSpotify(MOOD.NEGATIVE);
-			}
-			else if (score>0.25){
-				System.out.println("POSITIVE");
-				openSpotify(MOOD.POSITIVE);
+			
+		}
+			
+		double score = total/absTotal;
+		System.out.println(score);
+		if (score<-0.25) {
+			System.out.println("NEGATIVE");
+		//	openSpotify(MOOD.NEGATIVE);
+			return MOOD.NEGATIVE;
+		}
+		else if (score>0.25){
+			System.out.println("POSITIVE");
+		//	openSpotify(MOOD.POSITIVE);
+			return MOOD.POSITIVE;
 
-			}
-			else {
-				System.out.println("NEUTRAL");
-				openSpotify(MOOD.NEUTRAL);
-			}
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		else {
+			System.out.println("NEUTRAL");
+		//	openSpotify(MOOD.NEUTRAL);
+			return MOOD.NEUTRAL;
 		}
 		
 		
+	}
+	
+	private static List<String> getWordsFromFile(String stringfile) {
+		
+		List <String> wholeListOfWords = new ArrayList<String>();
+		FileReader fr;			
+		String wholeLine;
+
+		
+		try {
+			fr = new FileReader(stringfile);
+			BufferedReader br = new BufferedReader (fr);
+			while ((wholeLine=br.readLine())!=null){
+		        String parsedLine = wholeLine.replaceAll("[^a-zA-Z\\s]", "").replaceAll("\\s+", " ");
+				parsedLine = parsedLine.toLowerCase();
+		        String[] words = parsedLine.split(" ");
+				for(int r=0;r<words.length;r++){
+					wholeListOfWords.add(words[r]);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return wholeListOfWords;
+	}
+	
+	
+	public static MOOD start () {
+		List <String> wholeListOfWords = getWordsFromFile("story.txt");
+		getEmotion(wholeListOfWords);
+		
+
+		return MOOD.NEUTRAL;
+
 		
 		
+		
+
+	}
+
+
+
+
+	public static void main(String[] args) {
+		System.out.println("Mode = 1. Text -> music");
+		System.out.println("2. Web -> music");
+		System.out.println("3. Web -> web (API)");
+		if (args[0].equals("1")){
+			MoodText.start();
+		}
+		else if (args[0].equals("2")){
+			//Handler webHandler = new Handler();
+		}
+		else if (args[0].equals("3")){
+			
+		}
+			
+		
+	
 
 	}
 
